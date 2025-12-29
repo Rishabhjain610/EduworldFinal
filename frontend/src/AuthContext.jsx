f1cb8df5b0f3ab8a580003a4a5e6bc7464e176ab
Rishabh Jain <jainrishabh2610@gmail.com>
2025-12-29 18:28:01 +0530

    backendlink

--- frontend/src/AuthContext.jsx
+++ frontend/src/AuthContext.jsx
@@ -1,7 +1,7 @@
 import React,{useContext,createContext} from 'react'
 export const AuthDataContext = createContext();
 const AuthContext = ({children}) => {
-  const serverUrl="http://localhost:8080";
+  const serverUrl="https://eduworldfinal-backend.onrender.com";
   const value={
     serverUrl
   }
