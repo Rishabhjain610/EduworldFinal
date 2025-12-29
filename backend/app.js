f32a87b103d055735494de5780b57975e7322673
Rishabh Jain <jainrishabh2610@gmail.com>
2025-12-29 19:32:50 +0530

    updates

--- backend/app.js
+++ backend/app.js
@@ -32,7 +32,7 @@ const server = http.createServer(app);
 
 const io = socketIo(server, {
   cors: {
-    origin: ["http://localhost:5173", "http://localhost:5174", "http://localhost:8080", process.env.FRONTEND_URL || "http://localhost:5173"],
+    origin: ["http://localhost:5173", "http://localhost:5174", "http://localhost:8080", process.env.FRONTEND_URL || "http://localhost:5173","https://eduworld-rzjc.onrender.com"],
     methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true
   }
